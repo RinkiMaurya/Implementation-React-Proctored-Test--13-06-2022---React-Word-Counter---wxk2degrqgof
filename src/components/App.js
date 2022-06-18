ecaba184855d91826d8c5fb23cd9a179b7380f4d
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-06-18 12:40:34 +0530

    word count

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,49 @@ import React from "react";
 import { useState } from "react";
 import "../styles/App.css";
 const App = () => {
-  const [text, settext] = useState("");
-  const [max, setmax] = useState(50);
-  const [word, setword] = useState(0);
-  const [char, setchar] = useState(0);
+  const [fontSize, setFontSize] = useState(16);
+  const [max, setMax] = useState(50);
+  const [word, setWord] = useState(0);
+  const [char, setChar] = useState(0);
   // const font={}
-  const handlechange = (e) => {
-    settext(e.target.value);
-    setchar(text.split(" ").length);
+  const onChangHandler = (e) => {
+    console.log(e.target.value);
+    const text = e.target.value;
+
+    let w_count = 0;
+    if (text.length > 0) w_count = text.trim().split(" ").length;
+    setWord(w_count);
+
+    // set char count
+    setChar(text.length);
   };
+  // settext(e.target.value);
+  // setchar(text.split(" ").length);
 
   return (
     <div id="main">
       <textarea
-        style={{ fontSize: { word } }}
+        style={{ fontSize: fontSize + "px" }}
         maxLength={max}
-        onChange={handlechange}
+        onChange={onChangHandler}
       ></textarea>
-      <div id="word-counter">Total no of word written {char}</div>
-      <div id="char-counter">Total no of characters used {text.length}</div>
+      <div id="word-counter">Total no of word written {word}</div>
+      <div id="char-counter">Total no of characters used {char}</div>
       <input
         type="number"
         value={max}
-        onChange={(e) => setmax(e.target.value)}
+        onChange={(e) => setMax(e.target.value)}
         id="char-limit-input"
       />
       <input
-        id="fontSize-input"
         type="range"
+        id="fontSize-input"
+        min="16px"
         max="32px"
-        min="16"
-        onChange={(e) => setword(e.target.value)}
+        value={fontSize}
+        onChange={(e) => {
+          setFontSize(e.target.value);
+        }}
       />
     </div>
   );
